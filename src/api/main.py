375e6af6e28517f2a049c68f9fdd0753fee83bf2
Manny <hi@mannyparra.com>
2021-05-01 15:31:09 -0600

    fix .gitignore

--- src/api/main.py
+++
@@ -1,9 +0,0 @@
-from youtube_statistics import youTubeStats
-from config import API_KEY
-
-
-channel_id0: str = 'UCvtRTOMP2TqYqu51xNrqAzg'
-
-youtube = youTubeStats(API_KEY, channel_id0)
-youtube.get_channel_stats()
-
